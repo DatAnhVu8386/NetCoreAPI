3ee871d359d8e9b245a0ecd87dbb6c0f0d6b5487
DatAnhVu8386 <DatAnhVu04012003@gamil.com>
2025-02-23 20:14:22 +0700

    BTH17/2

---
+++ Demo_MVC/Models/BMI.cs
@@ -0,0 +1,11 @@
+
+using System.ComponentModel.DataAnnotations;
+using System.Security.Cryptography;
+
+public class BMI 
+{
+    public int Height { get; set; }
+    public int Weight { get; set; }
+    
+}
+
