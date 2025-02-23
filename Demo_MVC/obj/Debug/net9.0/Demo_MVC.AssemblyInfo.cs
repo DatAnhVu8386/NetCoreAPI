1114e3f161c9bfda26bdc9546cebf313f48dbdd9
DatAnhVu8386 <DatAnhVu04012003@gamil.com>
2025-02-23 23:33:16 +0700

    BTH17/2

--- Demo_MVC/obj/Debug/net9.0/Demo_MVC.AssemblyInfo.cs
+++ Demo_MVC/obj/Debug/net9.0/Demo_MVC.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Demo_MVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3ee871d359d8e9b245a0ecd87dbb6c0f0d6b5487")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+323dac4fc025977478e47e32713583c9fd611929")]
 [assembly: System.Reflection.AssemblyProductAttribute("Demo_MVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Demo_MVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
